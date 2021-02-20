5fc237597dbe44396a4f2c0e2e7db92bf3eb95d7
Marcos Jordao <marcosjordao@gmail.com>
2021-02-19 21:44:33 -0300

    feat: add log to retry

--- src/main/kotlin/com/marcosjordao/resiliencepoc/common/resilience/RetryFactory.kt
+++ src/main/kotlin/com/marcosjordao/resiliencepoc/common/resilience/RetryFactory.kt
@@ -3,15 +3,20 @@ package com.marcosjordao.resiliencepoc.common.resilience
 import io.github.resilience4j.retry.Retry
 import io.github.resilience4j.retry.RetryConfig
 import io.github.resilience4j.retry.RetryRegistry
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.Duration
 
 @Component
 class RetryFactory(
-    @Value("ibge.location.retryMaxAttempts") private val maxAttempts: Int,
-    @Value("ibge.location.retryWaitDuration") private val waitDuration: Long
+    @Value("\${resilience.retry.maxAttempts}") private val maxAttempts: Int,
+    @Value("\${resilience.retry.waitDuration}") private val waitDuration: Long
 ) {
+
+    private companion object {
+        private val log = KotlinLogging.logger { RetryFactory::class.java }
+    }
 
     fun buildRetry(name: String): Retry {
         val config = RetryConfig.custom<RetryConfig>()
@@ -21,7 +26,13 @@ class RetryFactory(
 
         val registry = RetryRegistry.of(config)
 
-        return registry.retry(name)
+        val retry = registry.retry(name)
+        retry.eventPublisher
+            .onEvent {
+                log.info { "Retry event '${it.eventType}' [name=${it.name}; attempts={${it.numberOfRetryAttempts}/$maxAttempts}]" }
+            }
+
+        return retry
     }
 
 }
